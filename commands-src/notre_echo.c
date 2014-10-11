61418d22cf0ca438de6b2f52c30818f176a17d78
Olmo Debenath <olmodebenath@gmail.com>
2014-10-11 12:01:47 +0200

    New command "echo"

---
+++ commands-src/notre_echo.c
@@ -0,0 +1,46 @@
+/*
+ * notre_echo.c
+ * 
+ * Copyright 2014 Loup <loup@Vroum>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFERSIZE 512
+
+int main(int argc, char **argv)
+{
+	int i;
+	for( i = 1; i < argc; i++ )
+	{
+	    printf( "%s ", argv[i] );
+	}
+	printf( "\n" );
+	
+	return 0;
+}
+
